3d788bfedcc499407a8820ec09d14fd20d10376b
Cause Chung <cuzfrog@gmail.com>
2026-05-06 08:34:03 -0400

    Refine setenv

--- mount_tmpfs.sh
+++ mount_tmpfs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo mount -t tmpfs -o size=8G,noatime tmpfs ./target
+
+# To run passwordless, add below to /etc/sudoers (use visudo): 
+# username ALL=(ALL) NOPASSWD: /path/to/docent/mount_tmpfs.sh
